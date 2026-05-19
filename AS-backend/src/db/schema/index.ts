aa16c85d5201e9a8a99e352e47877c4eea22c008
Sabyasachi Sahani <sibuspd@gmail.com>
2026-05-19 20:29:19 +0530

    Demo schema added temporary

---
+++ AS-backend/src/db/schema/index.ts
@@ -0,0 +1,15 @@
+import { timestamp } from "drizzle-orm/gel-core";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+//Defining the Demo Users table
+export const demoUsers = pgTable('demo_users', {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
+});
+
+//Export types for type-safe queries
+export type User = typeof demoUsers.$inferSelect;
+export type NewUser = typeof demoUsers.$inferInsert;
+
