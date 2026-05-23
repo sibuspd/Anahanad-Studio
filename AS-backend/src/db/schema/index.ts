74316a53035766a19cdd7af5777ed672fb42c4dd
Sabyasachi Sahani <sibuspd@gmail.com>
2026-05-24 02:27:37 +0530

    Index file updated to import from app.ts

--- AS-backend/src/db/schema/index.ts
+++ AS-backend/src/db/schema/index.ts
@@ -1,15 +1,16 @@
-import { timestamp } from "drizzle-orm/gel-core";
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+// import { timestamp } from "drizzle-orm/gel-core";
+// import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
-//Defining the Demo Users table
-export const demoUsers = pgTable('demo_users', {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    email: text('email').notNull(),
-    createdAt: timestamp('created_at').defaultNow().notNull()
-});
+// //Defining the Demo Users table
+// export const demoUsers = pgTable('demo_users', {
+//     id: serial('id').primaryKey(),
+//     name: text('name').notNull(),
+//     email: text('email').notNull(),
+//     createdAt: timestamp('created_at').defaultNow().notNull()
+// });
 
-//Export types for type-safe queries
-export type User = typeof demoUsers.$inferSelect;
-export type NewUser = typeof demoUsers.$inferInsert;
+// //Export types for type-safe queries
+// export type User = typeof demoUsers.$inferSelect;
+// export type NewUser = typeof demoUsers.$inferInsert;
 
+export * from './app'; // Extract schema from the file where it is defined
