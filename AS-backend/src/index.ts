9ec8afe12deeb4704389549287799dbe22d57810
Sabyasachi Sahani <sibuspd@gmail.com>
2026-05-25 21:31:15 +0530

    CORS configured for allowing frontend port to communicate with backend

--- AS-backend/src/index.ts
+++ AS-backend/src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import subjectsRouter from './routes/subject'; // Router imported for Subjects display
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
 
+// CORS handshake 
+app.use(cors( {
+    origin: process.env.FRONTEND_URL, // Requests from frontend origin allowed
+    methods: ['GET','POST','PUT','DELETE'], // Allowed methods
+    credentials: true // Allow cookies
+} ));
+
 app.use(express.json());
+
+app.use('/api/subjects',subjectsRouter );
 
 app.get('/', (req, res) => {
     res.send('Welcome to Anahanad Studio API');
@@ -12,3 +23,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, ()=> {
     console.log(`Server is running at  http://localhost:${PORT}`);
 });
+
