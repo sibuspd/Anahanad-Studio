58bacea8b9a5e7e149397cb87a59c762694950d3
Sabyasachi Sahani <sibuspd@gmail.com>
2026-06-04 13:27:21 +0530

    Created Route component for Adding classes

---
+++ AS-frontend/src/pages/classes/create.tsx
@@ -0,0 +1,10 @@
+// Route component responsible for creating a new class through a form
+import React from 'react'
+
+const Create = () => {
+  return (
+    <div>Create</div>
+  )
+}
+
+export default Create
