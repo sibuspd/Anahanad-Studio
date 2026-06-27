8ce12719f472e3e74d5999b5689d057862aca881
Sabyasachi Sahani <sibuspd@gmail.com>
2026-06-27 21:49:09 +0530

    Provided additional fields context to Better Auth instance w.r.t. authentication schemas

--- AS-backend/src/lib/auth.ts
+++ AS-backend/src/lib/auth.ts
@@ -3,13 +3,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db"; // your drizzle instance
+import * as schema from "../db/schema/auth";
 
 // Configuring Database Adapter for connecting to PostgreSQL 
 export const auth = betterAuth({
     secret: process.env.BETTER_AUTH_SECRET!, // Importing secret key from .env
     trustedOrigins: [process.env.FRONTEND_URL!], // Allowing frontend origin
     database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
+        provider: "pg", // or "mysql", "sqlite",
+        schema, // Drizzle schema containing authentication tables for recording user information on login
     }),
 
     // Setting up email and password Authentication methods
