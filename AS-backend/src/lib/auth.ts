560520c15278212fb4f6e751fa4a194463356453
Sabyasachi Sahani <sibuspd@gmail.com>
2026-06-27 21:31:08 +0530

    setting up additional fields for Better Auth request processing

--- AS-backend/src/lib/auth.ts
+++ AS-backend/src/lib/auth.ts
@@ -6,6 +6,8 @@ import { db } from "../db"; // your drizzle instance
 
 // Configuring Database Adapter for connecting to PostgreSQL 
 export const auth = betterAuth({
+    secret: process.env.BETTER_AUTH_SECRET!, // Importing secret key from .env
+    trustedOrigins: [process.env.FRONTEND_URL!], // Allowing frontend origin
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
@@ -13,5 +15,15 @@ export const auth = betterAuth({
     // Setting up email and password Authentication methods
     emailAndPassword: {
         enabled: true,
-    }
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: 'string', required: true, defaultValue: 'student', input: true, // To allow registration of new users
+            },
+            imageCldPubId: {
+                type: 'string', required: false, input: true, // To allow uploading of profile picture
+            },
+        }
+    },
 });
