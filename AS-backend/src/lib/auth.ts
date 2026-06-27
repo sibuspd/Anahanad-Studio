a8427a9662073c16e88b2b2b7c7f5b13bc1a6f1b
Sabyasachi Sahani <sibuspd@gmail.com>
2026-06-27 20:25:05 +0530

    Configured database for Better Auth

--- AS-backend/src/lib/auth.ts
+++ AS-backend/src/lib/auth.ts
@@ -1,8 +1,12 @@
 // CREATING A BETTER AUTH INSTANCE
 
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "../db"; // your drizzle instance
 
+// Configuring Database Adapter for connecting to PostgreSQL 
 export const auth = betterAuth({
-  //...
+    database: drizzleAdapter(db, {
+        provider: "pg", // or "mysql", "sqlite"
+    }),
 });
-
