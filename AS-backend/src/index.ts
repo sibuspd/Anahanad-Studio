4922cf043c93f626a8d19b1ec36a6cbcf7cd16c8
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-01 01:14:42 +0530

    registered classesRouter inside server file.

--- AS-backend/src/index.ts
+++ AS-backend/src/index.ts
@@ -6,6 +6,7 @@ AgentAPI.config(); // Agent to run the web app performance monitoring
 import express from 'express';
 import subjectsRouter from './routes/subject.js'; // Router imported for Subjects display
 import usersRouter from './routes/user.js'; // Router imported for Users display
+import classesRouter from './routes/classes.js'; // Router imported for creating new Sessions/Classes
 import cors from 'cors';
 import securityMiddleware from './middleware/security.js';
 import {toNodeHandler} from "better-auth/node"
@@ -37,7 +38,7 @@ app.use('/api/users', usersRouter);
 // app.use('/api/departments', departmentsRouter);
 // app.use('/api/courses', coursesRouter);
 // app.use('/api/batches', batchesRouter);
-// app.use('/api/class-sessions', classSessionsRouter);
+app.use('/api/class-sessions', classesRouter);
 // app.use('/api/enrollments', enrollmentsRouter);
 // app.use('/api/attendance', attendanceRouter);
 
