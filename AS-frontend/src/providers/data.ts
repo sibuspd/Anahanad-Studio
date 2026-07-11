ccb89cb635e1e2b99cd50e3aaf911946a2200c11
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-11 20:17:29 +0530

    Created helper function - resolveResource()

--- AS-frontend/src/providers/data.ts
+++ AS-frontend/src/providers/data.ts
@@ -1,40 +1,3 @@
-// import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-// import { API_URL } from "./constants";
-// export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-//   apiURL: API_URL,
-// });
-
-// import { DataProvider, BaseRecord, GetListParams, GetListResponse} from "@refinedev/core";
-
-// import { MOCK_SUBJECTS } from "@/constants/mock-data";
-
-// export const dataProvider: DataProvider = {
-//   getList: async <TData extends BaseRecord = BaseRecord>({
-//     resource
-//   }: GetListParams): Promise<GetListResponse<TData>> => {
-//     if (resource !== 'subjects') return { data: [] as TData[], total: 0 };
-
-//     return {
-//         data: MOCK_SUBJECTS as unknown as TData[],
-//         total: MOCK_SUBJECTS.length,
-//     }
-//   },
-
-//   getOne: async () => {
-//     throw new Error("This function is not present in mock");
-//   },
-//   create: async () => {
-//     throw new Error("This function is not present in mock");
-//   },
-//   update: async () => {
-//     throw new Error("This function is not present in mock");
-//   },
-//   deleteOne: async () => {
-//     throw new Error("This function is not present in mock");
-//   },
-
-//   getApiUrl: () => '',
-// };
 
 import { BACKEND_BASE_URL } from "@/constants"
 import { ListResponse, CreateResponse } from "@/types";
@@ -64,21 +27,31 @@ const buildHttpError = async (response: Response): Promise<HttpError> => {
   };
 };
 
+
+  const resolveResource = (resource: string) => {
+    switch (resource){
+      case "classes":
+        return "class-sessions";
+      default:
+        return resource;
+    } 
+  }
+
 /**  DATA PROVIDER OPTIONS */
 const options: CreateDataProviderOptions = {
   // >> OPTION 1: SIMPLE REST DATA PROVIDER
   getList: {
     getEndpoint: ({ resource } ) => {
-      // console.log("GET ENDPOINT:", resource);
-      return resource;
+      switch(resource){
+        case "classes":
+          return "class-sessions";
+        default:
+          return resolveResource(resource);
+      }
     },
 
     // Below is the default implementation of a function that returns the params to be sent to the backend
     buildQueryParams: async ( { resource, pagination, filters }) => {
-      // console.count("buildQueryParams");
-      // console.log("RESOURCE:", resource);
-      // console.log("PAGINATION:", pagination);
-      // console.log("FILTERS:", filters);
 
       const page = pagination?.currentPage ?? 1; // pagination decides which page to fetch
       const pageSize = pagination?.pageSize ?? 10; // pageSize decides how many items to fetch per page
@@ -120,7 +93,7 @@ const options: CreateDataProviderOptions = {
           break;
 
         /** CLASS SESSIONS */
-        case 'class-sessions':
+        case 'classes':
           if(field === 'courseId') params.courseId = value;
           if(field === 'batchId') params.batchId = value;
           if(field === 'teacherId') params.teacherId = value;
