49ce98838c4ce2d551795d9ea82988986bb9ae68
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-17 14:48:13 +0530

    Appended imports in dashboard index file

--- AS-frontend/src/components/dashboard/index.ts
+++ AS-frontend/src/components/dashboard/index.ts
@@ -1,2 +1,5 @@
 export { default as StatsCard } from "./stats-card";
 export { default as DashboardHeader } from "./dashboard-header";
+export { default as TodaysSessions } from "./todays-sessions";
+export { default as UpcomingSessions } from "./upcoming-sessions";
+export { default as RecentEnrollments } from "./recent-enrollments";
