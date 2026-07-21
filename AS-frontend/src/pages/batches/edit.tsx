fccfc5b10e86e63fb06b882dab4c288ec0dcfc48
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-21 13:12:54 +0530

    Updated Button text

--- AS-frontend/src/pages/batches/edit.tsx
+++ AS-frontend/src/pages/batches/edit.tsx
@@ -291,7 +291,7 @@ const EditBatch = () => {
                   {form.formState.isSubmitting && (
                     <Loader2 className="mr-2 h-4 w-4 animate-spin" />
                   )}
-                  Create Batch
+                  Update Batch
                 </Button>
               </div>
             </CardContent>
