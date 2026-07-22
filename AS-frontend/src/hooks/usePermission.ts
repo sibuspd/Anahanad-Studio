700cb14ca2307bf1222425c9b549bbd5d7b0a624
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-23 00:09:22 +0530

    Added "readonly" type to string[] received as argument in usePermission()

--- AS-frontend/src/hooks/usePermission.ts
+++ AS-frontend/src/hooks/usePermission.ts
@@ -2,7 +2,7 @@
 
 import { usePermissions } from "@refinedev/core";
 
-export const usePersmission = ( allowedRoles: string[]) => {
+export const usePersmission = ( allowedRoles: readonly string[]) => {
     const { data: role } = usePermissions<string>( {} );
 
     return allowedRoles.includes( role?? "");
