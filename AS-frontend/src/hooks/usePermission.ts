5fb60b4f29fd2f619f6ebd73547d654be16047a1
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-23 00:02:15 +0530

    Defined custom hook - usePermission()

---
+++ AS-frontend/src/hooks/usePermission.ts
@@ -0,0 +1,9 @@
+// Customized Hook to give access to Users based on Roles | Different from refine hook usePermissions()
+
+import { usePermissions } from "@refinedev/core";
+
+export const usePersmission = ( allowedRoles: string[]) => {
+    const { data: role } = usePermissions<string>( {} );
+
+    return allowedRoles.includes( role?? "");
+};
