29b32bcdfa04b9fb7945f4629dc68536cd7b630b
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-24 18:54:13 +0530

    Wrapped Signup page inside Auth-layout theme component

--- AS-frontend/src/pages/register/index.tsx
+++ AS-frontend/src/pages/register/index.tsx
@@ -1,7 +1,12 @@
+import AuthLayout from "@/components/auth/auth-layout";
 import SignUpForm from "@/components/auth/sign-up-form";
 
 const Register = () => {
-    return <SignUpForm />;
+  return (
+    <AuthLayout>
+      <SignUpForm />
+    </AuthLayout>
+  );
 };
 
 export default Register;
