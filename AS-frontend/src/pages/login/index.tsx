8c9f8b31e1106bf0e8b0f95a0101fda3292da516
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-24 18:51:38 +0530

    Wrapped login screen inside Auth-layout theme component

--- AS-frontend/src/pages/login/index.tsx
+++ AS-frontend/src/pages/login/index.tsx
@@ -1,7 +1,12 @@
 import SignInForm from "@/components/auth/sign-in-form";
+import AuthLayout from "@/components/auth/auth-layout";
 
 const Login = () => {
-    return <SignInForm />;
+    return (
+        <AuthLayout>
+            <SignInForm />
+        </AuthLayout>
+    );
 };
 
 export default Login;
