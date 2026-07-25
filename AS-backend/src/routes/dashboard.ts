b7fcb4479a3cccd8711aa1be00adae6bee65dac5
Sabyasachi Sahani <sibuspd@gmail.com>
2026-07-25 16:06:59 +0530

    Fixed typescript issue with 'today'

--- AS-backend/src/routes/dashboard.ts
+++ AS-backend/src/routes/dashboard.ts
@@ -14,7 +14,7 @@ import {
 import { user } from "../db/schema/auth";
 
 const router = express.Router();
-const today = new Date().toISOString().split("T")[0];
+const today: string = new Date().toISOString().split("T")[0]!;
 
 console.log("Dashboard router loaded");
 
